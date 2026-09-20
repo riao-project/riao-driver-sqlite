f56dfc4a233a32c695ebc5e5f3ca0b1368e7ac2a
Drew Immerman <drewimmerman@gmail.com>
2026-09-20 11:12:20 -0400

    feat: intersect operator
    riao-dbal#109

--- src/query-builder.ts
+++ src/query-builder.ts
@@ -3,6 +3,7 @@ import {
 	DatabaseFunctions,
 	DatabaseQueryBuilder,
 	Expression,
+	SelectQuery,
 } from '@riao/dbal';
 import { SqliteBuilder } from './sql-builder';
 import { DatabaseFunction } from '@riao/dbal/functions/function-token';
@@ -14,6 +15,24 @@ export class SqliteQueryBuilder extends DatabaseQueryBuilder {
 
 	protected getSqlType() {
 		return SqliteBuilder;
+	}
+
+	// SQLite doesn't support wrapping outer SELECT in parentheses for INTERSECT
+	// with WHERE clauses (syntax error)
+	protected shouldWrapIntersectQuery(): boolean {
+		return false;
+	}
+
+	// SQLite's parser also rejects parentheses around inner SELECT statements
+	// with WHERE clauses. Override to inline the query without wrapping.
+	public intersectWithSubquery(query: SelectQuery, all = false): this {
+		this.sql.trimEnd(' ');
+		this.sql.append(all ? ' INTERSECT ALL ' : ' INTERSECT ');
+		// Inline the query without Subquery wrapping (no parentheses)
+		this.select(query);
+		this.sql.space();
+
+		return this;
 	}
 
 	public override date(fn: DatabaseFunction): this {
@@ -90,5 +109,10 @@ export class SqliteQueryBuilder extends DatabaseQueryBuilder {
 			set: { [options.column]: options.value },
 			where: { [`${options.table}.${key}`]: columnName('NEW.' + key) },
 		});
+	}
+
+	public override intersectAll(query: SelectQuery): this {
+		// SQLite doesn't support INTERSECT ALL
+		throw new Error('SQLite does not support INTERSECT ALL. Use INTERSECT instead.');
 	}
 }
